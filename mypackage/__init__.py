0c8759053b6593de2eb644516f3910c1815bd908
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2025-05-08 09:34:43 +0000

    Apply ruff formater.

--- mypackage/__init__.py
+++ mypackage/__init__.py
@@ -1,7 +1,14 @@
-from .geometry import Point, Line
-from .utils import distance
+from .geometry import Point, Line, distance
+
 
 def personal_message():
     print("Hello from John Doe!")
 
+
 __version__ = "0.1.0"
+
+__all__ = [
+    "Point",
+    "Line",
+    "distance",
+]
--- mypackage/geometry/__init__.py
+++ mypackage/geometry/__init__.py
@@ -1,1 +1,7 @@
-from .geometry import Point, Line
+from .geometry import Point, Line, distance
+
+__all__ = [
+    "Point",
+    "Line",
+    "distance",
+]
